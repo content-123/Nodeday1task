8ff3b4b0bc2e369aee0bec73862cce12e3ba90d3
content-123 <keerthanakk10@gmail.com>
2023-12-19 22:57:20 +0530

    firstcommit

--- index.js
+++ index.js
@@ -1,60 +1,41 @@
-const fs= require("fs");
+
+const fs = require('fs').promises;
+const express = require('express');
+const app = express();
+const port = 8080;
 
 
+// Endpoint to create a text file with the current timestamp
+app.post('/Folder', async (req, res) => {
+  try {
+    const currentDate = new Date();
+    const formatedDate = `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1)
+      .toString()
+      .padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')}`;
+    const formatedTime = `${currentDate.getHours()}${currentDate.getMinutes()}${currentDate.getSeconds()}`;
 
-// const currentDate = new Date()
-// const formatedDate = `${currentDate.getFullYear()}-${(currentDate.getMonth() + 1).toString().padStart(2, '0')}-${currentDate.getDate().toString().padStart(2, '0')}`
-// const formatedTime = `${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}`
-// console.log(currentDate)
+    const fileName = `${formatedDate}_${formatedTime}.txt`;
+    const timestamp = currentDate.toISOString();
 
-// const fileName = `${formatedDate}_${formatedTime}.txt`
-// console.log(fileName)
+    await fs.writeFile(`./Folder/${fileName}`, `TimeStamp: ${timestamp}`);
+    res.status(201).json({ message: 'File created successfully', fileName });
+  } catch (error) {
+    console.error('Error creating file:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-// const timestamp = currentDate.toISOString()
-// const content = `TimeStamp: ${timestamp} `
-// console.log(content)
+// Endpoint to retrieve all text files in the folder
+app.get('/getAllFiles', async (req, res) => {
+  try {
+    const files = await fs.readdir(`./Folder`);
+    res.json({ files });
+  } catch (error) {
+    console.error('Error reading files:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-// fs.writeFile(`./${fileName}`, content, (err) => {
-//         if (err) console.log(err)
-//         console.log(`File: ${fileName} created Successfully`)
-//     })
-
-let ts = Date.now();
-
-let date_ob = new Date(ts);
-let date = date_ob.getDate();
-let month = date_ob.getMonth() + 1;
-let year = date_ob.getFullYear();
-
-// prints date & time in YYYY-MM-DD format
-let datee= year + "-" + month + "-" + date
-console.log(datee);
-
-let hours=date_ob.getHours();
-let minutes=date_ob.getMinutes();
-let seconds=date_ob.getSeconds();
-let time=hours + ":" + minutes + ":" + seconds
-console.log(time)
-
-let fileName=`${datee}_${time}`
-console.log(fileName)
-
-const currentDate = new Date()
-const timestamp = currentDate.toISOString()
-const content = `TimeStamp: ${timestamp} `
-console.log(content)
-
-
-
-fs.writeFile(`./${fileName}.txt`,"hello",()=>{
-  console.log("completed")
-})
-
-// fs.writeFile(`./${fileName}.txt`, content, (err) => {
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log(`File "${fileName}" has been written successfully.`);
-//     }
-//   });
-
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
